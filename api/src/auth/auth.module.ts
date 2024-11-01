80c9c7c7e7b1db8244159964e3af3de67d72a3ae
pidepiper009 <88837112+pidepiper009@users.noreply.github.com>
2024-11-01 12:38:48 +1100

    add api

---
+++ api/src/auth/auth.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { UserModule } from 'src/user/user.module';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './guards/jwt.strategy';
+import { JwtGuard } from './guards/jwt.guards';
+
+@Module({
+  imports: [
+    UserModule,
+    JwtModule.registerAsync({
+      useFactory: () => ({
+        secret: 'secret',
+        signOptions: { expiresIn: '3600s' },
+      }),
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService, JwtGuard, JwtStrategy],
+})
+export class AuthModule {}
