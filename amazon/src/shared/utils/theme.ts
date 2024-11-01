25fab5d373d563d0e120690d9bf3960c15c305c5
pidepiper009 <88837112+pidepiper009@users.noreply.github.com>
2024-11-01 17:31:16 +1100

    frontend initial homepage, signin, signup

---
+++ amazon/src/shared/utils/theme.ts
@@ -0,0 +1,14 @@
+import { createTheme } from "@mui/material/styles";
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#FF9900',
+      contrastText: '#0000000',
+    },
+    secondary: {
+      main: '#0000000',
+      contrastText: '#FFFFFF',
+    },
+  }
+});
