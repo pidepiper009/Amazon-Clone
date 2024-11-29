ffa34b95be0c64fa6efa7c6b9caafbebbef6c87c
pidepiper009 <88837112+pidepiper009@users.noreply.github.com>
2024-11-29 21:38:49 +1100

    created signin form tests

--- amazon/src/features/auth/components/SigninForm.component.tsx
+++ amazon/src/features/auth/components/SigninForm.component.tsx
@@ -129,6 +129,10 @@ const SigninFormComponent: FC = () => {
           />
 
           <Button
+            disabled={
+              !validatePasswordLength(password) ||
+              !validateEmail(email)
+            }
             variant='contained'
             style={{
               marginTop: '16px',
