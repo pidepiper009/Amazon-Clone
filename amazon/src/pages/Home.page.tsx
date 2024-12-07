c018ce8b85633488f88aabb7e18491f90a7a7925
pidepiper009 <88837112+pidepiper009@users.noreply.github.com>
2024-12-07 12:11:03 +1100

    created homepage header

--- amazon/src/pages/Home.page.tsx
+++ amazon/src/pages/Home.page.tsx
@@ -1,27 +1,14 @@
 import { logout, selectedUser } from "../features/auth/authSlice";
+import HeaderComponent from "../features/products/components/Header.component";
 import { useAppDispatch, useAppSelector } from "../hooks/redux/hooks";
 
 const HomePage = () => {
-  const dispatch = useAppDispatch();
 
-  const { user, jwt } = useAppSelector(selectedUser);
-
-  const logoutHandler = () => {
-    dispatch(logout());
-  }
-
-  return <div>
-    <h1>HomePage</h1>
-    <a onClick={logoutHandler} style={{ 
-      backgroundColor:'yellow', 
-      cursor:'pointer',
-      height: '40px',
-      width: '60px',
-      padding: '8px' }}>
-        Logout
-      </a>
-    {user?.email}
-  </div>
-}
+  return (
+    <div>
+      <HeaderComponent />
+    </div>
+  );
+};
 
 export default HomePage;
