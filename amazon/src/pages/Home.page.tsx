fa05fe4e2f3915835dbd20657f6b5d2b233d2643
pidepiper009 <88837112+pidepiper009@users.noreply.github.com>
2024-12-08 09:15:53 +1100

    created product component, added to homepage

--- amazon/src/pages/Home.page.tsx
+++ amazon/src/pages/Home.page.tsx
@@ -1,12 +1,38 @@
+import { useEffect } from "react";
 import { logout, selectedUser } from "../features/auth/authSlice";
 import HeaderComponent from "../features/products/components/Header.component";
+import ProductComponent from "../features/products/components/Product.component";
+import { getProducts } from "../features/products/productSlice";
 import { useAppDispatch, useAppSelector } from "../hooks/redux/hooks";
 
 const HomePage = () => {
 
+  const { cart, products } = useAppSelector((state) => state.product);
+
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(getProducts())
+  }, [])
+  
+
   return (
     <div>
       <HeaderComponent />
+      <div
+      style={{
+        display: 'flex',
+        flexWrap: 'wrap',
+        gap: '48px',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: '48px',
+      }}>
+        {products.length > 0 &&
+        products.map((product) => (
+          <ProductComponent key={product._id} product={product}/>
+        ))}
+      </div>
     </div>
   );
 };
